6c55d9171eca4700a9153aa7c7e357854b10c57b
Levik Naparstek <levik010@gmail.com>
2019-02-20 18:47:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
 def my_collect(array)
-# i = 0 
-# collection = []
-# while i < array
-# array.upcase 
-# array.upcase << collection
+i = 0 
+collection = []
+while i < array
+array.upcase 
+array.upcase << collection
  
-# i += 1
+i += 1
  
-# end
+end
  
   
   
