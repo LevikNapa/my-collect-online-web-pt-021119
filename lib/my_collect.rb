df2dcd22fcd92d5ee90c679cafd62a69a3f60f93
Levik Naparstek <levik010@gmail.com>
2019-02-20 19:02:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -19,7 +19,7 @@ def my_collect(array)
   i = 0
   name_collection = []
   while i < array.length
-    name_collection << yield array[i] end
+    name_collection << yield array[i] 
 
     i += 1
   end
